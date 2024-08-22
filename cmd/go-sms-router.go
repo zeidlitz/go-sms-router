54dbc81fcd40daa30ab820613d222290f87b11bf
zeidlitz <zeidlitzerik@gmail.com>
2024-08-22 10:42:43 +0200

    Initial implementation
    Included in this change:
    
      - Setting up env.go to handle environment variable parsing
      - Setting up some skeleton code for go-sms-router.go to understand the
        basic implementations

--- cmd/go-sms-router.go
+++ cmd/go-sms-router.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+ "net/url"
+ "io"
+ "log/slog"
+ "net/http"
+ "bytes"
+ "strconv"
+ "github.com/zeidlitz/go-sms-router/internal/env"
+)
+
+type Config struct {
+	username string
+	password string
+  sender string
+}
 
 func main() {
-  fmt.Print("Hello World!")
+
+    var cfg Config
+    cfg.username = env.GetString("USERNAME", "")
+    cfg.password = env.GetString("PASSWORD", "")
+    cfg.sender = env.GetString("SENDER", "DefaultSender")
+
+
+    message := "Hello 👋"
+    slog.Info("Attempting to send message", "message", message)
+
+    data := url.Values{
+        "from": {cfg.sender},
+        "to": {"+46738923036"},
+        "message":{message}}
+
+    req, err := http.NewRequest("POST", "https://api.46elks.com/a1/SMS", bytes.NewBufferString(data.Encode()))
+    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+    req.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+    req.SetBasicAuth(cfg.username, cfg.password)
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+
+    if err != nil {
+      slog.Info("Error when sending request", "ERROR:", err.Error())
+    }
+
+    defer resp.Body.Close()
+    body, err := io.ReadAll(resp.Body)
+
+    if err != nil {
+      slog.Info("Error when reading response", "ERROR:", err.Error())
+    }
+
+    slog.Info(string(body))
 }
